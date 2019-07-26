9c55fcfdef0234d6d79deef490f24f27b785f6eb
Anna Yu <annayu2010@gmail.com>
2019-07-25 21:48:58 -0700

    Customer Placing The Largest Number of Orders

---
+++ CustomerPlacingTheLargestNumberOfOrders.sql
@@ -0,0 +1,35 @@
+Query the customer_number from the orders table for the customer who has placed the largest number of orders.
+
+It is guaranteed that exactly one customer will have placed more orders than any other customer.
+
+The orders table is defined as follows:
+
+| Column            | Type      |
+|-------------------|-----------|
+| order_number (PK) | int       |
+| customer_number   | int       |
+| order_date        | date      |
+| required_date     | date      |
+| shipped_date      | date      |
+| status            | char(15)  |
+| comment           | char(200) |
+
+Sample Input
+| order_number | customer_number | order_date | required_date | shipped_date | status | 
+| 1            	| 1               		| 2017-04-09 | 2017-04-13    | 2017-04-12   | Closed |         
+| 2            	| 2               		| 2017-04-15 | 2017-04-20    | 2017-04-18   | Closed |         
+| 3            	| 3               		| 2017-04-16 | 2017-04-25    | 2017-04-20   | Closed |         
+| 4           	 | 3              		 | 2017-04-18 | 2017-04-28    | 2017-04-25   | Closed |         
+
+Sample Output
+| customer_number |
+| 3               |
+
+# Write your MySQL query statement below
+select customer_number
+from orders
+group by customer_number
+order by count(*) desc
+limit 1
+
+
