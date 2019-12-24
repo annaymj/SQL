a7b4ebd93f2887892e4f77786092548918196254
Anna Yu <annayu2010@gmail.com>
2019-12-23 19:42:03 -0800

    Weather Type In Each Country

---
+++ WeatherTypeInEachCountry.sql
@@ -0,0 +1,10 @@
+select c.country_name, 
+case 
+    when avg(w.weather_state) <= 15 then "Cold"
+    when avg(w.weather_state) >= 25 then "Hot"
+    else "Warm"
+end as "weather_type"
+from Countries c inner join Weather w
+on c.country_id = w.country_id
+where w.day between "2019-11-01" and "2019-11-30"
+group by w.country_id
