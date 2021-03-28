b61b626554109ee11a6f07f1f352a39209f38d95
Anna Yu <annayu2010@gmail.com>
2021-03-28 15:15:42 -0700

    Create Avg_processing_time_per_machine.sql

---
+++ Avg_processing_time_per_machine.sql
@@ -0,0 +1,45 @@
+```
+Activity table:
++------------+------------+---------------+-----------+
+| machine_id | process_id | activity_type | timestamp |
++------------+------------+---------------+-----------+
+| 0          | 0          | start         | 0.712     |
+| 0          | 0          | end           | 1.520     |
+| 0          | 1          | start         | 3.140     |
+| 0          | 1          | end           | 4.120     |
+| 1          | 0          | start         | 0.550     |
+| 1          | 0          | end           | 1.550     |
+| 1          | 1          | start         | 0.430     |
+| 1          | 1          | end           | 1.420     |
+| 2          | 0          | start         | 4.100     |
+| 2          | 0          | end           | 4.512     |
+| 2          | 1          | start         | 2.500     |
+| 2          | 1          | end           | 5.000     |
++------------+------------+---------------+-----------+
+
+Result table:
++------------+-----------------+
+| machine_id | processing_time |
++------------+-----------------+
+| 0          | 0.894           |
+| 1          | 0.995           |
+| 2          | 1.456           |
++------------+-----------------+
+
+There are 3 machines running 2 processes each.
+Machine 0's average time is ((1.520 - 0.712) + (4.120 - 3.140)) / 2 = 0.894
+Machine 1's average time is ((1.550 - 0.550) + (1.420 - 0.430)) / 2 = 0.995
+Machine 2's average time is ((4.512 - 4.100) + (5.000 - 2.500)) / 2 = 1.456
+```
+# Write your MySQL query statement below
+
+SELECT 
+a.machine_id,
+ROUND(AVG(b.timestamp - a.timestamp),3) AS processing_time
+FROM
+Activity a inner join Activity b
+ON a.machine_id = b.machine_id 
+AND a.process_id = b.process_id
+WHERE
+a.activity_type = 'start' AND b.activity_type = 'end'
+GROUP BY a.machine_id
